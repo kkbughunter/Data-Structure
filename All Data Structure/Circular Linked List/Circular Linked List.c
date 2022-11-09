49f8eb78f5537859cd8dce769650f81dc4a477e1
Karthikeyan_A <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-11-09 01:08:22 +0000

    Update Circular Linked List.c

--- All Data Structure/Circular Linked List/Circular Linked List.c
+++ All Data Structure/Circular Linked List/Circular Linked List.c
@@ -7,13 +7,18 @@ int main(){
     head = (struct node *) malloc(sizeof(struct node));
 
     head->next = NULL;
-    add(head, 10);
-    add(head, 5);
-    add(head, 30);
-    delete(head);
-    add(head, 20);
+    add(head, 32);
+    add(head, 43);
+    add(head, 56);
+    add(head, 98);
+    add(head, 1);
+    display(head);
+    deletePos(head,32);
     display(head);
     sortL(head);
     display(head);
+    reverseList(head);
+    display(head);
     return 0;
 }
+
