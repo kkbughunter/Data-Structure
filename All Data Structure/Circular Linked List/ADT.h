f801afb3b29c472d86f1782728fc850cb11e6340
Karthikeyan_A <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-11-09 01:07:59 +0000

    Update ADT.h

--- All Data Structure/Circular Linked List/ADT.h
+++ All Data Structure/Circular Linked List/ADT.h
@@ -20,8 +20,7 @@ void insert(struct node *h, int data){
 }
 
 int length(struct node *h){
-    struct node *ptr;
-    ptr = h->next;
+    struct node *ptr = h->next;
     int len =0;
     while (ptr != h)   
     {
@@ -31,8 +30,7 @@ int length(struct node *h){
     return len;
 }
 
-void add(struct node *h, int data)
-{
+void add(struct node *h, int data){
     struct node *temp = (struct node *) malloc(sizeof(struct node));
     temp->data = data;
     if(h->next == NULL){
@@ -50,8 +48,7 @@ void add(struct node *h, int data)
 }
 
 void display(struct node *h){
-    struct node *ptr;
-    ptr = h->next;
+    struct node *ptr = h->next;
     while (ptr != h)   
     {
         printf("%d ",ptr->data);
@@ -65,7 +62,29 @@ void delete(struct node *h){
     h->next = h->next->next;
     free(temp);
 }
+void deletePos(struct node *header, int key)
+{
 
+	struct node *ptr = header->next,*temp;
+	temp = header->next;
+
+	if(temp->data == key) {
+		header->next = header->next->next;
+		return;
+	}
+	while(ptr!=NULL){
+		if(ptr->data == key){
+			temp->next=ptr->next;
+			free(ptr);
+            return;
+		}
+		else{
+			temp = ptr;
+			ptr = ptr->next;
+
+		}
+	}
+}
 void sortL(struct node *head){
 	int i=0, j=0, len = length(head);
 	struct node *temp1=head->next, *temp = head->next;
@@ -81,4 +100,21 @@ void sortL(struct node *head){
 		temp1 = temp1->next;
 		temp = head->next;
 	}
+}
+
+void reverseList(struct node *h) {
+	struct node *ptr, *after, *prev, *last;
+	prev=h->next;
+	last=prev;
+	ptr=prev->next;
+	while(ptr->next!=h)
+	{
+		after=ptr->next;
+		ptr->next=prev;
+		prev=ptr;
+		ptr=after;
+	}
+	ptr->next=prev;
+	h->next=ptr;
+	last->next=h;
 }
