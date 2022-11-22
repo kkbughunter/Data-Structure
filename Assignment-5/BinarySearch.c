a418c76fc5dedff0edb2090edd6ad6251e896385
Karthikeyan_A <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-11-22 01:55:18 +0000

    Update BinarySearch.c

--- Assignment-5/BinarySearch.c
+++ Assignment-5/BinarySearch.c
@@ -3,32 +3,47 @@
 #include <stdlib.h>
 #include"BinarySearchADT.h"
 int main() {
-  struct node *temp ,*t = NULL;
-  t = insert(t, -31);
-  t = insert(t, 1);
-  t = insert(t, 2);
-  t = insert(t, 3);
-  t = insert(t, 4);
-  t = insert(t, 5);
-  t = insert(t, 6);
-  t = insert(t, 71);
+  	struct node *temp ,*t = NULL;
+  	t = insert(t, 29);
+  	t = insert(t, 23);
+  	t = insert(t, 4);
+  	t = insert(t, 13);
+  	t = insert(t, 39);
+  	t = insert(t, 31);
+  	t = insert(t, 45);
+	t = insert(t, 56);
+	t = insert(t, 49);
 
-  printf("Inorder traversal: ");
-  inorder(t);
+  	printf("Inorder traversal: ");
+  	inorder(t);
+  	printf("\nAfter deleting 3\n");
+  	t = deleteNode(t, 13);
+	printf("Inorder traversal: ");
+	inorder(t);
+  	printf("\npreorder traversal: ");
+  	preorder(t);
+  	printf("\npostorder traversal: ");
+  	postorder(t);
 
-  printf("\nAfter deleting 3\n");
-  t = deleteNode(t, 3);
+  	temp = minValueNode(t);
+  	printf("\nMin value = %d ",temp->data);
+  	temp = maxValueNode(t);
+  	printf("\nMax value = %d ",temp->data);
 
 
-  printf("Inorder traversal: ");
-  inorder(t);
-  printf("\npreorder traversal: ");
-  preorder(t);
-  printf("\npostorder traversal: ");
-  postorder(t);
+	int key =4;
+	temp = search(t,key);
+  	(temp == NULL) ? printf("\n Key Not Found\n") : printf("\nKey Found :)\n");
 
-  temp = minValueNode(t);
-  printf("\nmin value = %d ",temp->data);
-  temp = maxValueNode(t);
-  printf("\nmin value = %d ",temp->data);
+
+	inorder(t);
+	int arr[]={0},i=0;
+	int len = arrayInorder(t,arr,0);
+	printf("\nArray Element : ");
+	for(int i=0; i<len; i++)
+		printf("%d ",arr[i]);
+
+
+
+	return 0;
 }
