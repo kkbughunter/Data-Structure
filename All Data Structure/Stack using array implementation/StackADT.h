677dc286f623edcd0fac0a4c500620df28ed742a
Karthikeyan_A <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-12-15 15:16:21 +0000

    Update StackADT.h

--- All Data Structure/Stack using array implementation/StackADT.h
+++ All Data Structure/Stack using array implementation/StackADT.h
@@ -1,57 +1,63 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct stack{
-	int arr[40];
+struct stack {
+	int arr[100];
 	int size,top;
 };
+int isEmpty(struct stack *S){
+	if(S->top == -1) return 1;
+	else return 0;
+}
 
-int checkfil(struct stack *S){
+
+int isFill(struct stack *S){
 	if(S->top == S->size-1)
 		return 1;
 	return 0;
 }
 
-int checkempty(struct stack *S){
-	if(S->top == -1)
-		return 1;
-	return 0;
+int gettop(struct stack *S){
+	if(isEmpty(S))
+		return -1;
+	else return S->arr[S->top];
 }
 
-void CreateStack(struct stack *h, int lim){
 
-	h->size = lim;
-	h->top = -1;
+struct stack * createStack(int lim)
+{
+	struct stack *s = (struct stack *) malloc(sizeof(struct stack));
+	s->size = lim;
+	s->top = -1;
+	return s;
 }
 
 void push(struct stack *S, int x)
 {
-	if(checkfil(S)){
-		printf("FULL\n");
+	if(isFill(S)){
+		printf("FULL");
+		return ;
 	}
-	else {
-		S->top++;
-		S->arr[S->top] = x;
-	}
+	S->top++;
+	S->arr[S->top] = x;
 
 }
 
-void Display(struct stack *S)
+
+void displayStack(struct stack *S)
 {
 	struct stack *ptr=S;
-	for(int i=0; i<S->top+1; i++)
+	for(int i=0; i<S->top+1; i++){
 		printf("%d ",ptr->arr[i]);
-	printf("\n");
+	}
+
 }
+
 
 void pop (struct stack *S)
 {
-	if(checkempty(S))
+	if(isEmpty(S) == 1)
 		return ;
-	else
-		S->top--;
+	S->top--;
 }
-
-
-
 
