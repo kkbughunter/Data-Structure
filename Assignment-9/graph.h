0ec79c4f9c34d097c981ad493bb58e2411b23bc5
Karthikeyan_A <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-12-15 14:52:58 +0000

    Update graph.h

--- Assignment-9/graph.h
+++ Assignment-9/graph.h
@@ -1,43 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include"Queue.h"
 
 struct graph{
     int vertex;
     int edges;
     int **adj;
-    char **data;
-    int *visit;
+    char data[100][20];
 };
 
 struct graph* create_graph(){
-    int u,v,i,data;
+    int i,j,k,data;
     struct graph *g;
     g = (struct graph *) malloc(sizeof(struct graph));
     printf("Enter the number of Nodes and Edges (N,E) : ");
     scanf("%d %d",&g->vertex,&g->edges);
     g->adj = (int **) malloc(g->vertex * sizeof(int));
-    g->data = (char **) malloc(g->vertex * sizeof(char));
-    g->visit = (int *) malloc(g->vertex * sizeof(int));
+    g->adj[0] = (int *) malloc(g->vertex * sizeof(int));
 
-    // data part
     for(i=0; i<g->vertex; i++){
-        g->data[i] = (char *) malloc(5 * sizeof(char));
-        printf("Enter the data %d : ",i+1);
-        scanf("%s",g->data[i]);
-        g->visit[i] = 0;
-    }
-    // adj matrix
-    for(u=0; u<g->vertex; u++){
-        g->adj[u] = (int *) malloc(g->vertex * sizeof(int));
-        for(v=0; v<g->vertex; v++){
-            g->adj[u][v] = 0;
-        }
+        g->adj[i] = (int *) malloc(g->vertex * sizeof(int));
+        for(j=0; j<g->vertex; j++)
+            g->adj[i][j] = 0;
     }
     printf("Enter the nodes in pair (a -> b) \n");
     for(i=0; i<g->edges; i++){
-        scanf("%d %d",&u,&v);
-        g->adj[u-1][v-1] = 1;
+        scanf("%d",&j);
+        scanf("%d",&k);
+        g->adj[j-1][k-1] = 1;
     }
+    printf("Enter the all data : \n");
+    for(int i=0; i<g->vertex; i++)
+        scanf("%s",g->data[i]);
     return g;
 }
 
@@ -45,53 +39,91 @@ void displayadj(struct graph *g){
     int u,v;
     printf("Adj : matrix \n");
     for(u=0; u<g->vertex; u++){
-        for(v=0; v<g->vertex; v++){
+        for(v=0; v<g->vertex; v++)
             printf("%d ",g->adj[u][v]);
-        }
         printf("\n");
-    }
-    printf("\nData : matrix \n");
-    for(u=0; u<g->vertex; u++){
-        printf("%s ",g->data[u]);
     }
 }
 
 void BFS(struct graph *g, int start){
-    start--;
+
+    int i=0,visit[g->vertex];
+    for(i=0; i<g->vertex; i++) visit[i]=0;
+    struct queue *q = createQueue(20);
+    printf("\n\n");
     printf("BFS : ");
-    int i,deq;
-    struct queue *q = createQueue(50);
-    printf("%s,",g->data[start]);    // to print data use : g->data[start]
-    
-    g->visit[start] = 1;
+    start--;
+    printf("%s --> ",g->data[start]);
+    visit[start] = 1;
     enqueue(q,start);
     while (!isEmpty(q))
     {
-        deq = dequeue(q);
+        int deq = dequeue(q);
         for(i=0; i<g->vertex; i++){
-            if(g->adj[deq][i] == 1 && g->visit[i]==0){
-                printf("%s,",g->data[i]);       // to print data use : g->data[i]
-                g->visit[i] = 1;
+            if(g->adj[deq][i] == 1 && visit[i]==0){
+                printf("%s --> ",g->data[i]);
+                visit[i] = 1;
                 enqueue(q,i);
             }
         }
     }
+    printf("*\n\n");
 }
-
 /*
+Test case 1
 7 6
-1
-2
-3
-4
-5
-6
-7
 1 2
 1 3
 2 4
 2 5
 3 6
 3 7
+A
+B
+C
+D
+E
+F
+G
+
+*/
+
+/*
+Test case 2
+6 7
+1 2
+2 3
+2 4
+4 5
+5 6
+3 6
+3 1
+1
+2
+3
+4
+5
+6
+
+
+*/
+
+/*
+Test case 3
+6 7
+1 6
+6 2
+6 5
+5 4
+4 3
+2 3
+2 1
+Chennai
+Oouty
+Selam
+Ambur
+Theni
+Maduri
+
 
 */
