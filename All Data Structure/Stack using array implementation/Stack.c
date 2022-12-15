416acf2e18aa686d06ce0a0715a0ac00cf5ecbae
Karthikeyan_A <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-12-15 15:17:15 +0000

    Update Stack.c

--- All Data Structure/Stack using array implementation/Stack.c
+++ All Data Structure/Stack using array implementation/Stack.c
@@ -1,24 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include "StackADT.h"
+#include"Stack.h"
+ 
+int main(){
 
-int main()
-{
-	struct stack *head = (struct stack *) malloc(sizeof(struct stack));
-
-	CreateStack(head,3);
-	push(head, 2);
-	push(head, 4);
-	push(head, 6);
-	push(head, 6);
-	Display(head);
-
-	pop(head);
-	pop(head);
-	pop(head);
-	pop(head);
-	pop(head);
-	push(head, 6);
-	Display(head);
-	return 0;
+    struct stack *s = createStack(5);
+    push(s,10);
+    push(s,20);
+    pop(s);
+    pop(s);
+    pop(s);
+    pop(s);
+    push(s,20);
+    push(s,10);
+    push(s,10);
+    push(s,10);
+    push(s,10);
+    displayStack(s);
+    return 0;
 }
