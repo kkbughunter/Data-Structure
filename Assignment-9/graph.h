97114bb0826a0ad7ba89066b787d7b9e8b567675
Karthikeyan_A <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-12-15 22:30:56 +0530

    Add files via upload

---
+++ Assignment-9/graph.h
@@ -0,0 +1,180 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include"Queue.h"
+#include "Stack.h"
+
+struct graph{
+    int vertex;
+    int edges;
+    int **adj;
+    char data[100][20];
+};
+
+struct graph* create_graph(){
+    int i,j,k,data;
+    struct graph *g;
+    g = (struct graph *) malloc(sizeof(struct graph));
+    printf("Enter the number of Nodes and Edges (N,E) : ");
+    scanf("%d %d",&g->vertex,&g->edges);
+    g->adj = (int **) malloc(g->vertex * sizeof(int));
+    g->adj[0] = (int *) malloc(g->vertex * sizeof(int));
+
+    for(i=0; i<g->vertex; i++){
+        g->adj[i] = (int *) malloc(g->vertex * sizeof(int));
+        for(j=0; j<g->vertex; j++)
+            g->adj[i][j] = 0;
+    }
+    printf("Enter the nodes in pair (a -> b) \n");
+    for(i=0; i<g->edges; i++){
+        scanf("%d",&j);
+        scanf("%d",&k);
+        g->adj[j-1][k-1] = 1;
+    }
+    printf("Enter the all data : \n");
+    for(int i=0; i<g->vertex; i++)
+        scanf("%s",g->data[i]);
+    return g;
+}
+
+void displayadj(struct graph *g){
+    int u,v;
+    printf("Adj : matrix \n");
+    for(u=0; u<g->vertex; u++){
+        for(v=0; v<g->vertex; v++)
+            printf("%d ",g->adj[u][v]);
+        printf("\n");
+    }
+}
+
+void BFS(struct graph *g, int start){
+
+    int i=0,visit[g->vertex];
+    for(i=0; i<g->vertex; i++) visit[i]=0;
+    struct queue *q = createQueue(20);
+    printf("\n\n");
+    printf("BFS : ");
+    start--;
+    printf("%s --> ",g->data[start]);
+    visit[start] = 1;
+    enqueue(q,start);
+    while (!isEmpty(q))
+    {
+        int deq = dequeue(q);
+        for(i=0; i<g->vertex; i++){
+            if(g->adj[deq][i] == 1 && visit[i]==0){
+                printf("%s --> ",g->data[i]);
+                visit[i] = 1;
+                enqueue(q,i);
+            }
+        }
+    }
+    printf("*\n\n");
+}
+/*
+Test case 1
+7 6
+1 2
+1 3
+2 4
+2 5
+3 6
+3 7
+A
+B
+C
+D
+E
+F
+G
+
+*/
+
+/*
+Test case 2
+6 7
+1 2
+2 3
+2 4
+4 5
+5 6
+3 6
+3 1
+1
+2
+3
+4
+5
+6
+
+
+*/
+
+/*
+Test case 3
+6 7
+1 6
+6 2
+6 5
+5 4
+4 3
+2 3
+2 1
+Chennai
+Oouty
+Selam
+Ambur
+Theni
+Maduri
+
+
+*/
+
+void DFS(struct graph *g, int start){
+    int i=0,visit[g->vertex];
+    for(i=0; i<g->vertex; i++) visit[i]=0;
+    start--;
+    struct stack *s = createStack(20);
+    printf("%s --> ",g->data[start]);
+    visit[start] = 1;
+    push(s,start);
+    while (!isEmptystack(s))
+    {
+        int top = gettop(s),flag=0;
+        for(i=0; i<g->vertex; i++){
+            if(g->adj[top][i] == 1 && visit[i]==0){
+                flag=1;
+                break;
+            }
+        }
+        if(flag){
+            printf("%s --> ",g->data[i]);
+            visit[i] = 1;
+            push(s,i);
+        }
+        else pop(s);
+    }
+    printf("*\n\n");
+    
+}
+
+
+
+/*
+7 7
+1 2
+1 3
+3 2
+3 4
+4 5
+4 7
+6 5
+1
+2
+3
+4
+5
+6
+7
+
+
+*/
